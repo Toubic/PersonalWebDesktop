7c80a5154282e487e54671dbbacb6ca6edb5d47f
Tobias Johansson <tj22fe@student.lnu.se>
2016-01-29 05:57:05 +0100

    ta bort overflodig kod + grundstrukturen for att testa chatten

---
+++ examination/client/source/js/Chat.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function Chat(aWindow){
+
+    this.aDiv = aWindow;
+    this.address = "ws://vhost3.lnu.se:20080/socket/";
+    this.aKey = "eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd";
+
+}
+
+Chat.prototype.connect = function(){
+
+}
+
+Chat.prototype.send = function(){
+
+}
+Chat.prototype.receive = function(){
+
+}
--- examination/client/source/js/app.js
+++ examination/client/source/js/app.js
@@ -7,6 +7,3 @@ var theDiv = document.querySelector("#theSetup");
 var aMemory = new NewMemory(theDiv);
 aMemory.readyUp();
 
-var theDiv2 = document.querySelector("#theSetup2");
-var aMemory2 = new NewMemory(theDiv2);
-aMemory2.readyUp();
