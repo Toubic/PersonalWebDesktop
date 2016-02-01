8865be8d66cb14f948d337622805bdf7c4585624
Tobias Johansson <tj22fe@student.lnu.se>
2016-02-01 03:43:11 +0100

    +timestamp pa meddelanden

--- examination/client/source/js/Chat.js
+++ examination/client/source/js/Chat.js
@@ -51,7 +51,7 @@ Chat.prototype.send = function(message){
 Chat.prototype.receive = function(theData){
 
     var dFragment = document.createDocumentFragment();
-    var theText = document.createTextNode(theData.username + ": ");
+    var theText = document.createTextNode(theData.username + ":");
     var pUsername = document.createElement("p");
     pUsername.appendChild(theText);
     dFragment.appendChild(pUsername);
@@ -59,6 +59,12 @@ Chat.prototype.receive = function(theData){
     var pMessage = document.createElement("p");
     pMessage.appendChild((theText));
     dFragment.appendChild(pMessage);
+    var timestamp = new Date();
+    timestamp = timestamp.toTimeString();
+    theText = document.createTextNode(timestamp);
+    var pTimestamp = document.createElement("p");
+    pTimestamp.appendChild((theText));
+    dFragment.appendChild(pTimestamp);
     this.aDiv.previousElementSibling.appendChild(dFragment);
 
 };
