28a88dadf7e9bae02f9d27a37b89c97139372d96
Tobias Johansson <tj22fe@student.lnu.se>
2016-02-07 10:24:54 +0100

    skicka meddelanden med textarea vilket kravs

--- examination/client/source/js/Chat.js
+++ examination/client/source/js/Chat.js
@@ -5,7 +5,6 @@ function Chat(aWindow, aUsername){
     this.aDiv = aWindow.nextElementSibling;
     this.address = "ws://vhost3.lnu.se:20080/socket/";
     this.aKey = "eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd";
-    this.sendButton = this.aDiv.querySelectorAll("input")[0];
     this.wSocket = null;
     this.aUsername = aUsername;
 
@@ -17,10 +16,11 @@ Chat.prototype.connect = function(){
 
     this.wSocket = new WebSocket(this.address);
 
-    this.sendButton.addEventListener("click", function(){
-        var message = this.aDiv.querySelectorAll("textarea")[0].value;
-        this.send(message);
-
+    this.aDiv.querySelectorAll("textarea")[0].addEventListener("keypress", function(event){
+        if(event.keyCode === 13) {
+            var message = this.aDiv.querySelectorAll("textarea")[0].value;
+            this.send(message);
+        }
     }.bind(this));
 
     this.wSocket.addEventListener("message", function(event){
